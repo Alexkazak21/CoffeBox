5a27bbe53ee3aefdbd41f0184cf4c8f8f353c2f8
Filchenko Nikita <filchenko.niki@mail.ru>
2024-11-13 23:44:39 +0300

    feat: add service part

--- CoffeBox/CoffeBox.cpp
+++ CoffeBox/CoffeBox.cpp
@@ -1,10 +1,19 @@
 ﻿#include <iostream>
+#include <stdlib.h>
+#include <format>
 
 using namespace std;
+
 void showMenu();
 void showCoinMenu();
 double getMoney(double userBalance);
 double Payment(double balance, double price);
+void ServiceProcess(int pincode, double* balance, int* cups);
+void ClearConsole();
+bool CheckPINCODE(int pin);
+void ShowServiceMenu(double* balance, int* cups);
+int AddCups();
+void Withdrow(double* balance);
 
 int main()
 {
@@ -12,12 +21,16 @@ int main()
 
     int choice = 0;
     int cupCount = 3;
+    int* cupsPtr{ &cupCount };
     double userBalance = 0.0;
     double boxBalance = 0.0;
+    double* boxBalancePtr = { &boxBalance };
     double money = 0.0;
     const double CAPPUCINO_PRICE = 2.0;
     const double LATTE_PRICE = 3.0;
     const double ECPRESSO_PRICE = 1.5;
+
+    const int PIN = 1234;
 
     while (true) {
         if (cupCount == 0)
@@ -53,8 +66,9 @@ int main()
             cout << "Ok, take your espresso" << endl;
             cupCount--;
         }
-        else if (choice == 5) {
-            cout << "Zaglushka dlia Service" << endl;
+        else if (choice == 5)
+        {
+            ServiceProcess(PIN, boxBalancePtr, cupsPtr);
         }
         else {
             cout << "Input [1..5], please" << endl;
@@ -87,6 +101,9 @@ void showCoinMenu()
 double getMoney(double balance)
 {
     int choice = 0;
+
+    ClearConsole();
+
     while (true)
     {
         cout << "Balance: " << balance << endl;
@@ -98,7 +115,7 @@ double getMoney(double balance)
         {
             balance += 0.1;
         }
-        else if (choice == 2) 
+        else if (choice == 2)
         {
             balance += 0.2;
         }
@@ -123,6 +140,7 @@ double getMoney(double balance)
         }
     }
 }
+
 double Payment(double balance, double price)
 {
     if (balance < price)
@@ -130,5 +148,131 @@ double Payment(double balance, double price)
         cout << "Insufficient money" << endl;
         return 2;
     }
+  
     return  balance - price;
+}
+
+void ServiceProcess(int pincode, double* balance, int* cups)
+{
+    int choice = 0;
+    bool switcher = true;
+    while (switcher)
+    {
+        ClearConsole();
+
+        cout << "Select what to do\n";
+        cout << "1) Input pin\n";
+        cout << "2) Back to Coffee selection\n\n";
+        cout << "Your choice: ";
+
+        cin >> choice;
+
+        if (choice < 1 || choice > 2)
+        {
+            cout << "\nWrong choice.\n";
+            system("pause");
+            continue;
+        }
+
+        switch (choice)
+        {
+        case 1:
+        {
+            if (!CheckPINCODE(pincode))
+            {
+                while (true)
+                {
+                    ClearConsole();
+                    cout << "ON MAINTENANCE\n\n";
+                    system("pause");
+                    continue;
+                }
+            }
+
+            while (switcher)
+            {
+                ClearConsole();
+                int servicemanChoise = 0;
+                ShowServiceMenu(balance, cups);
+
+                cout << "Insert your choice: ";
+                cin >> servicemanChoise;
+
+                if (servicemanChoise < 1 && servicemanChoise > 4)
+                {
+                    system("pause");
+                    continue;
+                }
+
+                switch (servicemanChoise)
+                {
+                case 1:
+                    *cups += AddCups();
+                    break;
+                case 2:
+                    Withdrow(balance);
+                    break;
+
+                default:
+                    switcher = false;
+                    break;
+                }
+            }
+        }
+        break;
+
+        default:
+            switcher = false;
+            break;
+        }
+    }
+}
+
+bool CheckPINCODE(int pin)
+{
+    for (int i = 0; i < 3; i++)
+    {
+        ClearConsole();
+
+        int tempCode = 0;
+        cout << "Insert PIN code to access coffee box." << endl;
+        cin >> tempCode;
+
+        if (tempCode == pin)
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+void ClearConsole()
+{
+    system("cls");
+}
+
+void ShowServiceMenu(double* balance, int* cups)
+{
+    printf("Balance %.2f BYN\n", *balance);
+    cout << "Cups: " << *cups << endl;
+
+    cout << "1) Add cups\n";
+    cout << "2) Withdrawal\n";
+    cout << "3) Back to Coffee selection\n\n";
+}
+
+void Withdrow(double* balance)
+{
+    *balance = 0.0;
+}
+
+int AddCups()
+{
+    ClearConsole();
+    int cupNumber = 0;
+    cout << "1) Input cups number: ";
+    cin >> cupNumber;
+
+    return cupNumber;
 }
